82cf6f83cba96aa69655fa0440b090d2f2eb30db
Hiroki Sato <hrs@allbsd.org>
2024-02-03 02:45:34 +0900

    Add ktermc termcap entry

--- termcap.kt
+++ termcap.kt
@@ -1,2 +1,6 @@
 vk|kterm|kterm kanji terminal emurator (X window system):\
 	:hs:es:ts=\E[?E\E[?%i%dT:fs=\E[?F:ds=\E[?H:tc=xterm:
+ktermc|kterm16c|kterm-16color|kanji terminal with 16 colors:\
+	:op=\E[m:pa#64:Co#8:AF=\E[3%dm:AB=\E[4%dm:\
+	:mb=\E[5m:md=\E[1m:mr=\E[7m:mh=\E[2m:me=\E[m:\
+	:tc=kterm:
