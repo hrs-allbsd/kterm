6b9ac84865f8dcbc142ea75c85ad6c69914d3fce
Hiroki Sato <hrs@allbsd.org>
2024-02-03 02:00:44 +0900

    scrollbar: Use SendMousePosition() if available

--- scrollbar.c
+++ scrollbar.c
@@ -49,6 +49,7 @@ extern int BackgroundPixmapIsOn;
 static void ScrollTextTo();
 static void ScrollTextUpDownBy();
 
+extern Boolean SendMousePosition();
 
 /* resize the text window for a terminal screen, modifying the
  * appropriate WM_SIZE_HINTS and taking advantage of bit gravity.
@@ -555,6 +556,9 @@ void HandleScrollForward (gw, event, params, nparams)
     XtermWidget w = (XtermWidget) gw;
     register TScreen *screen = &w->screen;
 
+    if (SendMousePosition(gw, event) != 0)
+	return;
+
     ScrollTextUpDownBy (gw, (XtPointer) NULL,
 			(XtPointer)params_to_pixels (screen, params, (int) *nparams));
     return;
@@ -570,6 +574,9 @@ void HandleScrollBack (gw, event, params, nparams)
 {
     XtermWidget w = (XtermWidget) gw;
     register TScreen *screen = &w->screen;
+
+    if (SendMousePosition(gw, event) != 0)
+	return;
 
     ScrollTextUpDownBy (gw, (XtPointer) NULL,
 			(XtPointer)-params_to_pixels (screen, params, (int) *nparams));
